38ca3548ad9e5a5c6c691443c0787fd38cefc1e7
itzadetunji <adetunjiadeyinka29@gmail.com>
2026-06-04 00:15:39 +0100

    Adjust positioning of the underline effect in Pricing component link for improved visual alignment on hover.

--- src/pages/home/components/Pricing.tsx
+++ src/pages/home/components/Pricing.tsx
@@ -193,7 +193,7 @@ export default function Pricing() {
 							className="text-teal-600 font-semibold hover:text-teal-700 transition-colors relative group"
 						>
 							<span>Contact our sales team</span>
-							<span className="absolute bottom-0 left-0 w-0 h-0.5 bg-teal-600 group-hover:w-full transition-all duration-300"></span>
+							<span className="absolute -bottom-0.75 left-0 w-0 h-0.5 bg-teal-600 group-hover:w-full transition-all duration-300"></span>
 						</Link>
 					</p>
 				</div>
